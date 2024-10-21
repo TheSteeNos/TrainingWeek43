09feba1172585e7d0e3e8f293893b1f178ab3ae2
TheSteeNos <davidsteens2001@gmail.com>
2024-10-21 12:08:02 +0200

    Configure

---
+++ TrainingWeek43.js
@@ -0,0 +1,77 @@
+/* -----------------------------------------------------------------------------
+    Task: A
+    create a function that "draws" a tree of a given height.
+    Example the following is a tree of height 5
+
+           5        *
+           4       ***
+           3      *****
+           2     *******
+           1        x
+*/
+
+console.log("");
+console.log("Task: A");
+console.log("");
+
+
+
+/* -----------------------------------------------------------------------------
+    Task: B
+
+    Write a function that "draws" an arrow of a given size.
+    Example: This is an arrow of size 3.
+
+    *
+    * *
+    * * *
+    * *
+    * 
+*/
+
+console.log("");
+console.log("Task: B");
+console.log("");
+
+
+
+/* -----------------------------------------------------------------------------
+    Task: C
+    Write a function that draws a box of n by m dimensions. Take into acount the diffrence in aspectratio.
+
+    Example: This is a aproximatly a 2 by 2 box. 
+    +--------+
+    |        |
+    |        |
+    +--------+
+*/
+
+console.log("");
+console.log("Task: C");
+console.log("");
+
+
+
+/* -----------------------------------------------------------------------------
+    Task: D
+    Write a function that returns true if a word is a Heterogram.   
+*/
+
+console.log("");
+console.log("Task: D");
+console.log("");
+
+
+
+/* -----------------------------------------------------------------------------
+    Task: E
+    Write a function that returns true if two words are Anagrams.
+*/
+
+console.log("");
+console.log("Task: E");
+console.log("");
+
+
+
+console.log("");
